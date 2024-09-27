d0d2de0fd666a913f0bf3cc5763e2024811543ce
Pauli Mäenpää <maenpaapauli@gmail.com>
2024-09-27 21:42:24 +0300

    reset

--- react_app/src/components/landing/Navigation.jsx
+++ react_app/src/components/landing/Navigation.jsx
@@ -4,10 +4,7 @@ function Navigation() {
     return (
       <nav id="navigation">
         <ul id="navigation-left-section">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
+
         </ul>
         <div id="navigation-right-section">
 
