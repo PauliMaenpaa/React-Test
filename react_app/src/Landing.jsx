9bf1d8da9eaab312fe7c960dcff048112f0f78ea
Pauli Mäenpää <maenpaapauli@gmail.com>
2024-09-27 21:23:48 +0300

    first commit

---
+++ react_app/src/Landing.jsx
@@ -0,0 +1,12 @@
+import './Landing.css'
+import Navigation from './components/landing/Navigation';
+
+function Landing() {
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default Landing
