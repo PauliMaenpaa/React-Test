a014665fafefb7247a98da27c9db18bead0cf4ed
Pauli Mäenpää <maenpaapauli@gmail.com>
2024-09-28 10:58:50 +0300

    changes

--- react_app/src/components/landing/Footer.jsx
+++ react_app/src/components/landing/Footer.jsx
@@ -1,9 +1,15 @@
 import './footer.css';
+import github from '../../img/github.png';
+import instagram from '../../img/instagram.png';
+import email from '../../img/email.png';
 
 function Footer() {
     return (
         <footer id='footer'>
             <p>© Example</p>
+            <img className='logo' src={github} alt="github-logo" />
+            <img className='logo' src={instagram} alt="instagram-logo" />
+            <img className='logo' src={email} alt="email-logo" />
         </footer>
     )
 }
